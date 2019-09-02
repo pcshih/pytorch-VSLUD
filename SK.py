70265ee7ddf9dabb3f3cef72169f9ccd3e55492f
pcshih <pcshih.cs07g@nctu.edu.tw>
2019-09-02 18:18:10 +0800

    can train about 13 epoch and die...

--- SK.py
+++ SK.py
@@ -1,12 +1,202 @@
 import torch
 import torch.nn as nn
-from FCSN import FCSN
+#from FCSN import FCSN
 
 import random
 
+
 class SK(nn.Module):
-    def __init__(self):
+    def __init__(self, n_class=2):
         super(SK, self).__init__()
+        # conv1 input shape (batch_size, Channel, H, W) -> (1,1024,1,T)
+        self.conv1_1 = nn.Conv2d(1024, 64, (1,3), padding=(0,100))
+        self.sn1_1 = nn.utils.spectral_norm(self.conv1_1)
+        self.bn1_1 = nn.BatchNorm2d(64)
+        self.relu1_1 = nn.ReLU(inplace=True)#nn.LeakyReLU(0.2)
+        self.conv1_2 = nn.Conv2d(64, 64, (1,3), padding=(0,1))
+        self.sn1_2 = nn.utils.spectral_norm(self.conv1_2)
+        self.bn1_2 = nn.BatchNorm2d(64)
+        self.relu1_2 = nn.ReLU(inplace=True)#nn.LeakyReLU(0.2)
+        self.pool1 = nn.MaxPool2d((1,2), stride=(1,2), ceil_mode=True)  # 1/2
+
+        # conv2
+        self.conv2_1 = nn.Conv2d(64, 128, (1,3), padding=(0,1))
+        self.sn2_1 = nn.utils.spectral_norm(self.conv2_1)
+        self.bn2_1 = nn.BatchNorm2d(128)
+        self.relu2_1 = nn.ReLU(inplace=True)#nn.LeakyReLU(0.2)
+        self.conv2_2 = nn.Conv2d(128, 128, (1,3), padding=(0,1))
+        self.sn2_2 = nn.utils.spectral_norm(self.conv2_2)
+        self.bn2_2 = nn.BatchNorm2d(128)
+        self.relu2_2 = nn.ReLU(inplace=True)#nn.LeakyReLU(0.2)
+        self.pool2 = nn.MaxPool2d((1,2), stride=(1,2), ceil_mode=True)  # 1/4
+
+        # conv3
+        self.conv3_1 = nn.Conv2d(128, 256, (1,3), padding=(0,1))
+        self.sn3_1 = nn.utils.spectral_norm(self.conv3_1)
+        self.bn3_1 = nn.BatchNorm2d(256)
+        self.relu3_1 = nn.ReLU(inplace=True)#nn.LeakyReLU(0.2)
+        self.conv3_2 = nn.Conv2d(256, 256, (1,3), padding=(0,1))
+        self.sn3_2 = nn.utils.spectral_norm(self.conv3_2)
+        self.bn3_2 = nn.BatchNorm2d(256)
+        self.relu3_2 = nn.ReLU(inplace=True)#nn.LeakyReLU(0.2)
+        self.conv3_3 = nn.Conv2d(256, 256, (1,3), padding=(0,1))
+        self.sn3_3 = nn.utils.spectral_norm(self.conv3_3)
+        self.bn3_3 = nn.BatchNorm2d(256)
+        self.relu3_3 = nn.ReLU(inplace=True)#nn.LeakyReLU(0.2)
+        self.pool3 = nn.MaxPool2d((1,2), stride=(1,2), ceil_mode=True)  # 1/8
+
+        # conv4
+        self.conv4_1 = nn.Conv2d(256, 512, (1,3), padding=(0,1))
+        self.sn4_1 = nn.utils.spectral_norm(self.conv4_1)
+        self.bn4_1 = nn.BatchNorm2d(512)
+        self.relu4_1 = nn.ReLU(inplace=True)#nn.LeakyReLU(0.2)
+        self.conv4_2 = nn.Conv2d(512, 512, (1,3), padding=(0,1))
+        self.sn4_2 = nn.utils.spectral_norm(self.conv4_2)
+        self.bn4_2 = nn.BatchNorm2d(512)
+        self.relu4_2 = nn.ReLU(inplace=True)#nn.LeakyReLU(0.2)
+        self.conv4_3 = nn.Conv2d(512, 512, (1,3), padding=(0,1))
+        self.sn4_3 = nn.utils.spectral_norm(self.conv4_3)
+        self.bn4_3 = nn.BatchNorm2d(512)
+        self.relu4_3 = nn.ReLU(inplace=True)#nn.LeakyReLU(0.2)
+        self.pool4 = nn.MaxPool2d((1,2), stride=(1,2), ceil_mode=True)  # 1/16
+
+        # conv5
+        self.conv5_1 = nn.Conv2d(512, 512, (1,3), padding=(0,1))
+        self.sn5_1 = nn.utils.spectral_norm(self.conv5_1)
+        self.bn5_1 = nn.BatchNorm2d(512)
+        self.relu5_1 = nn.ReLU(inplace=True)#nn.LeakyReLU(0.2)
+        self.conv5_2 = nn.Conv2d(512, 512, (1,3), padding=(0,1))
+        self.sn5_2 = nn.utils.spectral_norm(self.conv5_2)
+        self.bn5_2 = nn.BatchNorm2d(512)
+        self.relu5_2 = nn.ReLU(inplace=True)#nn.LeakyReLU(0.2)
+        self.conv5_3 = nn.Conv2d(512, 512, (1,3), padding=(0,1))
+        self.sn5_3 = nn.utils.spectral_norm(self.conv5_3)
+        self.bn5_3 = nn.BatchNorm2d(512)
+        self.relu5_3 = nn.ReLU(inplace=True)#nn.LeakyReLU(0.2)
+        self.pool5 = nn.MaxPool2d((1,2), stride=(1,2), ceil_mode=True)  # 1/32
+
+        # fc6
+        self.fc6 = nn.Conv2d(512, 4096, (1,7))
+        self.sn6 = nn.utils.spectral_norm(self.fc6)
+        self.in6 = nn.InstanceNorm2d(4096)
+        self.relu6 = nn.ReLU(inplace=True)#nn.LeakyReLU(0.2)
+        self.drop6 = nn.Dropout2d(p=0.5)
+
+        # fc7
+        self.fc7 = nn.Conv2d(4096, 4096, (1,1))
+        self.sn7 = nn.utils.spectral_norm(self.fc7)
+        self.in7 = nn.InstanceNorm2d(4096)
+        self.relu7 = nn.ReLU(inplace=True)#nn.LeakyReLU(0.2)
+        self.drop7 = nn.Dropout2d(p=0.5)
+
+        self.score_fr = nn.Conv2d(4096, n_class, (1,1))
+        self.sn_score_fr = nn.utils.spectral_norm(self.score_fr)
+        self.bn_score_fr = nn.BatchNorm2d(n_class)
+        self.in_score_fr = nn.InstanceNorm2d(n_class)
+        self.relu_score_fr = nn.ReLU(inplace=True)#nn.LeakyReLU(0.2)
+        self.score_pool4 = nn.Conv2d(512, n_class, (1,1))
+        self.sn_score_pool4 = nn.utils.spectral_norm(self.score_pool4)
+        self.bn_score_pool4 = nn.BatchNorm2d(n_class)
+        self.relu_bn_score_pool4 = nn.ReLU(inplace=True)#nn.LeakyReLU(0.2)
+
+        self.upscore2 = nn.ConvTranspose2d(
+            n_class, n_class, (1,4), stride=(1,2))
+        self.sn_upscore2 = nn.utils.spectral_norm(self.upscore2)
+        self.bn_upscore2 = nn.BatchNorm2d(n_class)
+        self.relu_upscore2 = nn.ReLU(inplace=True)#nn.LeakyReLU(0.2)
+
+        self.upscore16 = nn.ConvTranspose2d(
+            n_class, n_class, (1,32), stride=(1,16))
+        self.sn_upscore16 = nn.utils.spectral_norm(self.upscore16)
+        self.bn_upscore16 = nn.BatchNorm2d(n_class)
+        self.relu_upscore16 = nn.ReLU(inplace=True)#nn.LeakyReLU(0.2)
+        self.sigmoid_upscore16 = nn.Sigmoid()
+        self.tanh_upscore16 = nn.Tanh()
+
+        self.relu_add = nn.ReLU()#nn.LeakyReLU(0.2)
+
+        self.softmax = nn.Softmax(dim=1)
+
+        self.conv_reconstuct1 = nn.Conv2d(n_class, 1024, (1,1))
+        self.bn_reconstruct1 = nn.BatchNorm2d(1024)
+        self.relu_reconstuct1 = nn.ReLU(inplace=True)
+
+        self.conv_reconstuct2 = nn.Conv2d(1024, 1024, (1,1))
+        self.bn_reconstruct2 = nn.BatchNorm2d(1024)
+        self.relu_reconstuct2 = nn.ReLU(inplace=True)
+
+
+    def forward(self, x):
+        # input
+        h = x
+        in_x = x
+        # conv1
+        h = self.relu1_1(self.bn1_1(self.conv1_1(h)))       #;print(h.shape)
+        h = self.relu1_2(self.bn1_2(self.conv1_2(h)))       #;print(h.shape)
+        h = self.pool1(h)                                   #;print(h.shape)
+        # conv2
+        h = self.relu2_1(self.bn2_1(self.conv2_1(h)))       #;print(h.shape)
+        h = self.relu2_2(self.bn2_2(self.conv2_2(h)))       #;print(h.shape)
+        h = self.pool2(h)                                   #;print(h.shape)
+        # conv3
+        h = self.relu3_1(self.bn3_1(self.conv3_1(h)))       #;print(h.shape)
+        h = self.relu3_2(self.bn3_2(self.conv3_2(h)))       #;print(h.shape)
+        h = self.relu3_3(self.bn3_3(self.conv3_3(h)))       #;print(h.shape)
+        h = self.pool3(h)                                   #;print(h.shape)
+        # conv4
+        h = self.relu4_1(self.bn4_1(self.conv4_1(h)))       #;print(h.shape)
+        h = self.relu4_2(self.bn4_2(self.conv4_2(h)))       #;print(h.shape)
+        h = self.relu4_3(self.bn4_3(self.conv4_3(h)))       #;print(h.shape)
+        h = self.pool4(h)                                   #;print(h.shape)
+        pool4 = h
+        # conv5
+        h = self.relu5_1(self.bn5_1(self.conv5_1(h)))       #;print(h.shape)
+        h = self.relu5_2(self.bn5_2(self.conv5_2(h)))       #;print(h.shape)
+        h = self.relu5_3(self.bn5_3(self.conv5_3(h)))       #;print(h.shape)
+        h = self.pool5(h)                                   #;print(h.shape)
+        # conv6
+        h = self.relu6(self.fc6(h))                         #;print(h.shape)
+        h = self.drop6(h)                                   #;print(h.shape)
+        # conv7
+        h = self.relu7(self.fc7(h))                         #;print(h.shape)
+        h = self.drop7(h)                                   #;print(h.shape)
+        # conv8
+        h = self.in_score_fr(self.score_fr(h)) # original should be bn_score_fr, in order to handle the one frame input i.e. [1,1024,1,1] input
+        # deconv1
+        h = self.upscore2(h)
+        upscore2 = h
+        # get score_pool4c to do skip connection
+        h = self.bn_score_pool4(self.score_pool4(pool4))
+        h = h[:, :, :, 5:5+upscore2.size()[3]]
+        score_pool4c = h
+        # skip connection
+        h = upscore2+score_pool4c
+        # deconv2
+        h = self.upscore16(h)
+        h = h[:, :, :, 27:27+x.size()[3]]; #print("before softmax:", h)
+
+        # h*100 getting larger differences(tricks!!)
+        h_softmax = self.softmax(h*50); #print("after softmax:", h_softmax)
+
+        # get simulated 0/1 vector
+        mask = h_softmax[:,1,:].view(1,1,1,-1); #print("mask:", mask) # [1,1,1,T] use key frame score to be the mask
+
+        h_mask = h*mask; #print("h_mask:", h_mask)
+
+        h_reconstruct = self.relu_reconstuct1(self.bn_reconstruct1(self.conv_reconstuct1(h_mask))) # [1,1024,1,T]
+        x_select = in_x*mask
+
+        # merge with input features
+        h_merge = h_reconstruct + x_select # [1,1024,1,T]
+        h_merge_reconstruct = self.relu_reconstuct2(self.bn_reconstruct2(self.conv_reconstuct2(h_merge))) # [1,1024,1,T]
+
+
+        return h_merge_reconstruct,mask,h   # [1,1024,1,T],[1,1,1,T],[1,2,1,T]
+
+
+class SK_old(nn.Module):
+    def __init__(self):
+        super(SK_old, self).__init__()
 
         self.FCSN = FCSN(n_class=2)
 
@@ -48,7 +238,8 @@ class SK(nn.Module):
 
         ###new###
         #index_mask = self.sigmoid(h[:,1]-h[:,0]).view(1,1,1,-1)
-        h_softmax = self.softmax(h*100) # [1,2,1,T]
+        diverse_h = h*100
+        h_softmax = self.softmax(diverse_h) # [1,2,1,T]
         index_mask = h_softmax[:,1,:].view(1,1,1,-1)
         #index_mask = self.sigmoid(h[:,1]-h[:,0]).view(1,1,1,-1)
         #index_mask = (indices==1).type(torch.float32)
@@ -85,7 +276,7 @@ if __name__ == '__main__':
     model.to(device)
     inp = torch.randn(1, 1024, 1, 5, requires_grad=True).to(device)
 
-    summary,mask = model(inp)
+    summary,mask,_ = model(inp)
     print(summary.shape)
     print(mask)
 
--- train.py
+++ train.py
@@ -23,6 +23,11 @@ PATH_model = "saved_models"
 
 EPOCH = 700
 
+# reconstruction error coefficient
+reconstruction_error_coeff = 0.0005
+# diversity error coefficient
+diversity_error_coeff = 1.5
+
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # ref: https://gist.github.com/jeasinema/ed9236ce743c8efaf30fa2ff732749f5
@@ -101,7 +106,7 @@ S_D = SD().to(device)
 
 
 optimizerS_K = optim.Adam(S_K.parameters(), lr=0.00001)
-optimizerS_D = optim.SGD(S_D.parameters(), lr=0.0002)
+optimizerS_D = optim.SGD(S_D.parameters(), lr=0.00004)
 
 
 # configure training record
@@ -185,7 +190,7 @@ for epoch in range(EPOCH):
         S_K.zero_grad()
 
         #S_K_summary,column_mask = S_K(vd)
-        S_K_summary,index_mask = S_K(vd)
+        S_K_summary,index_mask,_ = S_K(vd)
         output = S_D(S_K_summary)
         label = torch.full((1,), 1, device=device)
 
@@ -200,21 +205,77 @@ for epoch in range(EPOCH):
         ###old reconstruct###
 
         ###new reconstruct###
-        reconstruct_loss = torch.sum((S_K_summary-vd)**2 * index_mask) / torch.sum(index_mask)
+        #reconstruct_loss = torch.sum((S_K_summary-vd)**2 * index_mask) / torch.sum(index_mask) # [1,1024,1,S]-[1,1024,1,T]
         ###new reconstruct###
 
 
         # diversity
-        S_K_summary = index_mask*S_K_summary
-        S_K_summary_reshape = S_K_summary.view(S_K_summary.shape[1], S_K_summary.shape[3])
-        norm_div = torch.norm(S_K_summary_reshape, 2, 0, True)
-        S_K_summary_reshape = S_K_summary_reshape/norm_div
-        loss_matrix = S_K_summary_reshape.transpose(1, 0).mm(S_K_summary_reshape)
-        diversity_loss = loss_matrix.sum() - loss_matrix.trace()
-        #diversity_loss = diversity_loss/len(column_mask)/(len(column_mask)-1)
-        diversity_loss = diversity_loss/(torch.sum(index_mask))/(torch.sum(index_mask)-1)
+        # S_K_summary = index_mask*S_K_summary
+        # S_K_summary_reshape = S_K_summary.view(S_K_summary.shape[1], S_K_summary.shape[3])
+        # norm_div = torch.norm(S_K_summary_reshape, 2, 0, True)
+        # S_K_summary_reshape = S_K_summary_reshape/norm_div
+        # loss_matrix = S_K_summary_reshape.transpose(1, 0).mm(S_K_summary_reshape)
+        # diversity_loss = loss_matrix.sum() - loss_matrix.trace()
+        # #diversity_loss = diversity_loss/len(column_mask)/(len(column_mask)-1)
+        # diversity_loss = diversity_loss/(torch.sum(index_mask))/(torch.sum(index_mask)-1)
 
-        S_K_total_loss = errS_K+reconstruct_loss+diversity_loss # for summe dataset beta=1
+        ######################## LOSS FROM FCSN #########################
+        # 2D 1D conversion
+        outputs_reconstruct = S_K_summary.view(1,1024,-1) # [1,1024,1,T] -> [1,1024,T] 
+        mask = index_mask.view(1,1,-1) # [1,1,1,T] -> [1,1,T] 
+        feature = vd.view(1,1024,-1) # [1,1024,1,T] -> [1,1024,T] 
+
+        # reconst. loss改成分批再做平均
+        feature_select = feature*mask
+        outputs_reconstruct_select = outputs_reconstruct*mask
+        feature_diff_1 = torch.sum((feature_select-outputs_reconstruct_select)**2, dim=1)
+        feature_diff_1 = torch.sum(feature_diff_1, dim=1)
+
+        mask_sum = torch.sum(mask, dim=2)
+        mask_sum = torch.sum(mask_sum, dim=1)
+
+        reconstruct_loss = torch.mean(feature_diff_1/mask_sum)
+        
+
+        # diversity loss
+        batch_size, feat_size, frames = outputs_reconstruct.shape
+
+        outputs_reconstruct_norm = torch.norm(outputs_reconstruct, p=2, dim=1, keepdim=True)
+
+        normalized_outputs_reconstruct = outputs_reconstruct/outputs_reconstruct_norm
+
+        normalized_outputs_reconstruct_reshape = normalized_outputs_reconstruct.permute(0, 2, 1)
+
+        similarity_matrix = torch.bmm(normalized_outputs_reconstruct_reshape, normalized_outputs_reconstruct)
+
+        mask_trans = mask.permute(0,2,1)
+        mask_matrix = torch.bmm(mask_trans, mask)
+        # filter out non key
+        similarity_matrix_filtered = similarity_matrix*mask_matrix
+
+        diversity_loss = 0
+        acc_batch_size = 0
+        for j in range(batch_size):
+            batch_similarity_matrix_filtered = similarity_matrix_filtered[j,:,:]
+            batch_mask = mask[j,:,:]
+            if batch_mask.sum() < 2:
+                #print("select less than 2 frames", batch_mask.sum())
+                batch_diversity_loss = 0
+            else:
+                batch_diversity_loss = (batch_similarity_matrix_filtered.sum()-batch_similarity_matrix_filtered.trace())/(batch_mask.sum()*(batch_mask.sum()-1))
+                acc_batch_size += 1
+
+            diversity_loss += batch_diversity_loss
+
+        if acc_batch_size>0:
+            diversity_loss /= acc_batch_size
+            #print(acc_batch_size)
+        else:
+            diversity_loss = 0
+
+
+        S_K_total_loss = errS_K + reconstruction_error_coeff*reconstruct_loss + diversity_error_coeff*diversity_loss # for summe dataset beta=1
+        #S_K_total_loss = errS_K+reconstruct_loss
         S_K_total_loss.backward()
 
         # update
@@ -232,7 +293,7 @@ for epoch in range(EPOCH):
         err_S_D_real.backward()
 
         # fake summary #
-        S_K_summary,_ = S_K(vd)
+        S_K_summary,_,_ = S_K(vd)
         output = S_D(S_K_summary.detach()); #print(S_K_summary)
         label.fill_(0)
         err_S_D_fake = criterion(output, label)
@@ -245,8 +306,8 @@ for epoch in range(EPOCH):
         # record
         time_list.append(time.time())
         S_K_iter_loss_list.append(errS_K)
-        reconstruct_iter_loss_list.append(reconstruct_loss)
-        diversity_iter_loss_list.append(diversity_loss)
+        reconstruct_iter_loss_list.append(reconstruction_error_coeff*reconstruct_loss)
+        diversity_iter_loss_list.append(diversity_error_coeff*diversity_loss)
         S_D_real_iter_loss_list.append(err_S_D_real)
         S_D_fake_iter_loss_list.append(err_S_D_fake)
         S_D_total_iter_loss_list.append(S_D_total_loss)
